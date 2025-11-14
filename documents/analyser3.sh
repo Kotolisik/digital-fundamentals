56778d7492d44ec8fc0301e81faeb83f9b8f5eb3
Eremeeva Taisia Dmitrievna 4731204/50003 <e889rm@mail.ru>
2025-11-14 19:41:26 +0300

    Create analyser3.sh

---
+++ documents/analyser3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+road=~/papka/digital-fundamentals-tasks/DFM/LAB04/
+
+for nameF in $(ls $road)
+do
+        for entrails in $(cat $road$nameF)
+        do
+		if [[ $entrails =~ "@spbstu.ru" ]]
+		then
+                	klichka=$(echo $entrails | cut -d ":" -f1)
+               		starost=$(echo $entrails | cut -d ":" -f2)
+			pochka=$(echo $entrails | cut -d ":" -f3)
+                	echo -e "имя: $klichka\nвозраст: $starost\nпочта: $pochka\n"
+		fi
+        done
+
+done
